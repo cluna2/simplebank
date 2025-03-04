0924eeb7b34bce3d28add389cca01f726a595262
cluna2 <lunacris2739@gmail.com>
2025-03-04 15:32:31 -0800

    implement http api using Gin

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	db "github.com/cluna2/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// runs HTTP server on specified address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
