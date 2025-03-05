b10e852f7bd2c992027581520dccd0f68891f392
cluna2 <lunacris2739@gmail.com>
2025-03-04 19:39:53 -0800

    use viper to read config from file and env vars

---
+++ util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+// stores all config values of the app
+// read by viper from config file or env vars
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// reads config from file or env vars
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
