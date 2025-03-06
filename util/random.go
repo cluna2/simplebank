863ceb5ef7e4d1d0d6852a229a3074b103e4a687
cluna2 <lunacris2739@gmail.com>
2025-03-05 20:03:07 -0800

    Implement user queries with unit tests and account constraints

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 )
@@ -47,4 +48,9 @@ func RandomCurrency() string {
 
 func RandomID() int64 {
 	return RandomInt(0, 100)
+}
+
+// generates a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
