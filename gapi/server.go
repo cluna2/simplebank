da3c4c2947c5b0b9e3192daaee74f59f716e078d
cluna2 <44912466+cluna2@users.noreply.github.com>
2025-05-05 22:46:30 -0700

    Add grpc service and gateway to serve HTTP and GRPC requests (#15)

---
+++ gapi/server.go
@@ -0,0 +1,33 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/cluna2/simplebank/db/sqlc"
+	"github.com/cluna2/simplebank/pb"
+	"github.com/cluna2/simplebank/token"
+	"github.com/cluna2/simplebank/util"
+)
+
+// serves  gRPC requests for our banking service
+type Server struct {
+	pb.UnimplementedSimplebankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// creates a new gRPC server
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
