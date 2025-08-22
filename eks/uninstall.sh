39da75f035651d88ee3f6d8fd4e92358e1460ffb
cluna2 <lunacris2739@gmail.com>
2025-08-21 21:45:43 -0700

    Add install/uninstall scripts for deployment

---
+++ eks/uninstall.sh
@@ -0,0 +1,2 @@
+kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.1/cert-manager.yaml
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy.yaml
